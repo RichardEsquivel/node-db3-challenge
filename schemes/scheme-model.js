8a748e7047f0a5703524fff2fe08ed4464ac3346
RichardEsquivel <rlesquivel@gmail.com>
2019-10-09 14:52:03 -0500

    Executed a basic find on the database after connecting knex to the database

---
+++ schemes/scheme-model.js
@@ -0,0 +1,37 @@
+const knex = require('knex')
+const knexConfig = require('../knexfile')
+
+const db = knex(knexConfig.development);
+
+module.exports = {
+	find,
+	findById,
+	findSteps,
+	add,
+	update,
+	remove
+}
+
+function find() {
+	return db('schemes')
+}
+
+function findById() {
+	return db('schemes')
+}
+
+function findSteps() {
+	return db('schemes')
+}
+
+function add() {
+	return db('schemes')
+}
+
+function update() {
+	return db('schemes')
+}
+
+function remove() {
+	return db('schemes')
+}
