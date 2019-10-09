1c168a7cd1c42889903136482d6db6f9efc21942
RichardEsquivel <rlesquivel@gmail.com>
2019-10-09 16:48:18 -0500

    Completed find, findyById, findSteps, add and update in scheme-model.js

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -16,20 +16,40 @@ function find() {
 	return db('schemes')
 }
 
-function findById() {
-	return db('schemes')
+// findById(id):
+// Expects a scheme id as its only parameter.
+// Resolve to a single scheme object.
+// On an invalid id, resolves to null.
+
+function findById(id) {
+	return db('schemes').where({ id }).first();
 }
 
-function findSteps() {
-	return db('schemes')
+function findSteps(id) {
+	return db('steps as s')
+		.join('schemes as sc', 's.scheme_id', 'sc.id')
+		.select('s.id', 'sc.scheme_name', 's.step_number', 's.instructions')
+		.where('s.scheme_id', '=', id)
+		.orderBy('s.step_number')
 }
-
-function add() {
-	return db('schemes')
+//select * from schemes;
+//insert into schemes(scheme_name)Values('name');
+function add(data) {
+	return db('schemes').insert(data)
+		.then((id) => {
+			return findById(id[0])
+		})
 }
-
-function update() {
+//Update schemes
+//Set scheme_name = 'New Plan"
+//Where id =6
+function update(obj, id) {
 	return db('schemes')
+		.update('scheme_name', obj.scheme_name)
+		.where({ id })
+		.then(() => {
+			return findById(id)
+		})
 }
 
 function remove() {
